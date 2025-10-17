41b6505b9dc7f0c832df28635c8dbb46602cc961
Tiffany1xd <stefanyabril2006@gmail.com>
2025-10-16 19:28:29 -0500

    Limpiando archivos mal ubicados

--- src/peliculas/peliculas.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PeliculasController } from './peliculas.controller';
-import { PeliculasService } from './peliculas.service';
-
-@Module({
-  controllers: [PeliculasController],
-  providers: [PeliculasService]
-})
-export class PeliculasModule {}
