cd7dc66eca1603f3a9d0717d216ed6eed2d9fd8c
Tiffany1xd <stefanyabril2006@gmail.com>
2025-10-18 12:47:25 -0500

    DTOs, controladores y servicios de peliculas realizados

--- Taller 3/cine/src/peliculas/peliculas.module.ts
+++ Taller 3/cine/src/peliculas/peliculas.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { PeliculasController } from './peliculas.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PeliculasService } from './peliculas.service';
+import { PeliculasController } from './peliculas.controller';
+import { Pelicula } from './pelicula.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Pelicula])],
   controllers: [PeliculasController],
-  providers: [PeliculasService]
+  providers: [PeliculasService],
+  exports: [PeliculasService],
 })
 export class PeliculasModule {}
